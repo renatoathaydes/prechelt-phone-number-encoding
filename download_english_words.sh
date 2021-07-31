7f1e5eeb593d3d2028125aae4fe36833b7ba2a35
renatoathaydes <renato.athaydes@curity.io>
2021-07-31 14:54:25 +0200

    More benchmarks with large dictionary.

--- download_english_words.sh
+++ download_english_words.sh
@@ -2,5 +2,5 @@
 
 set -e
 curl https://raw.githubusercontent.com/dwyl/english-words/master/words.txt --output tmp.txt
-tail -n 200000 tmp.txt > words.txt
+tail -n 100000 tmp.txt > words.txt
 rm tmp.txt
