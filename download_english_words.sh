4f004fb85015692e8d138dff86f872b4d976b528
Renato Athaydes <renato@athaydes.com>
2021-08-07 07:25:19 +0200

    Build english-dict by skipping every 5 words in it. This allows all words selected to have a good a-z distribution while remaining under 100,000 words - too many words slow down programs too much.

--- download_english_words.sh
+++ download_english_words.sh
@@ -2,5 +2,7 @@
 
 set -e
 curl https://raw.githubusercontent.com/dwyl/english-words/master/words.txt --output tmp.txt
-tail -n 100000 tmp.txt > words.txt
+
+# the dictionary has 466k words, so we can skip every 5 words to get close to 100k
+cat tmp.txt | awk "NR % 5 == 0 && NR < 500001" > words.txt
 rm tmp.txt
