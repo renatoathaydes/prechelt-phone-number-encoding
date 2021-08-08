7ceba8976c4af5371c51051963eacd7a06377754
renatoathaydes <renato.athaydes@curity.io>
2021-08-08 19:16:38 +0200

    Include Java OpenJDK, GraalVM and native-image in benchmarks.

--- benchmark.sh
+++ benchmark.sh
@@ -8,24 +8,37 @@ set -e
 # If compilation is required, also add instructions to do that before "Generating INPUTS"
 #
 
+OPENJDK_JAVA=~/.sdkman/candidates/java/16.0.1-open/bin/java
+GRAALVM_JAVA=~/.sdkman/candidates/java/21.2.0.r16-grl/bin/java
+
 if [ ! -f words.txt ]; then
   echo "Downloading English Words Dictionary."
   ./download_english_words.sh
 fi
 
 COMMANDS=(
-  "java -cp build/java Main"          # Java 1
-  "java -cp build/java Main2"         # Java 2
+  "$OPENJDK_JAVA -cp build/java1 Main"          # Java 1
+  "$OPENJDK_JAVA -cp build/java2 Main2"         # Java 2
+  "$GRAALVM_JAVA -cp build/java1 Main"          # Java 1
+  "$GRAALVM_JAVA -cp build/java2 Main2"         # Java 2
+  ./graalvm-main
+  ./graalvm-main2
 #  "sbcl --script src/lisp/main.lisp"  # Common Lisp
-  "./phone_encoder"                   # Rust
+#  "./phone_encoder_std_hash"           # Rust
+#  "./phone_encoder"                    # Rust (ahash)
 #  "src/dart/phone-encoder/bin/phone_encoder.exe" # Dart
 #  "julia src/julia/phone_encoder.jl"  # Julia
 )
 
 echo "Compiling Java sources"
 rm -rf build || true
-javac src/java/*.java -d build/java
+javac src/java/Main.java -d build/java1
+javac src/java/Main2.java -d build/java2
 javac src/java/util/*.java -d build/util
+
+echo "Compiling Java native images"
+native-image -cp build/java1 Main graalvm-main
+native-image -cp build/java2 Main2 graalvm-main2
 
 echo "Compiling Rust sources"
 cd src/rust/phone_encoder && cargo build --release && cp target/release/phone_encoder ../../../
