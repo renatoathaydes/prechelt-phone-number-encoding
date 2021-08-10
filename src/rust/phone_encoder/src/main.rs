9b16c7b801db08a3096744d3b69da02ecc6be4c7
Renato Athaydes <renato@athaydes.com>
2021-08-10 18:32:11 +0200

    Use Rust stack-allocated struct to hold keys, avoiding dynamic allocations.

--- src/rust/phone_encoder/src/main.rs
+++ src/rust/phone_encoder/src/main.rs
@@ -3,16 +3,9 @@ use std::env::args;
 use std::fs::File;
 use std::io::{self, BufRead, BufWriter, Write};
 use std::path::Path;
+use std::hash::{Hash, Hasher};
 
-use lazy_static::lazy_static;
-use num_bigint::{BigUint, ToBigUint};
-
-type Dictionary = HashMap<BigUint, Vec<String>>;
-
-lazy_static! {
-    static ref ONE: BigUint = 1.to_biguint().unwrap();
-    static ref TEN: BigUint =10.to_biguint().unwrap();
-}
+type Dictionary = HashMap<DigitBytes, Vec<String>>;
 
 static DIGITS: [&str; 10] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
@@ -54,12 +47,11 @@ fn print_translations<'dict, W: Write>(
     if start >= digits.len() {
         return print_solution(num, &words, writer);
     }
-    let mut n = ONE.clone();
+    let mut key = DigitBytes::new();
     let mut found_word = false;
     for i in start..digits.len() {
-        n *= &*TEN;
-        n += nth_digit(digits, i);
-        if let Some(found_words) = dict.get(&n) {
+        key.push(nth_digit(digits, i));
+        if let Some(found_words) = dict.get(&key) {
             for word in found_words {
                 found_word = true;
                 words.push(word);
@@ -113,18 +105,17 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     Ok(io::BufReader::new(file).lines())
 }
 
-fn word_to_number(word: &str) -> BigUint {
-    let mut n = ONE.clone();
+fn word_to_number(word: &str) -> DigitBytes {
+    let mut key = DigitBytes::new();
     for ch in word.bytes().filter_map(char_to_digit) {
-        n *= &*TEN;
-        n += ch;
+        key.push(ch);
     }
-    n
+    key
 }
 
 fn nth_digit(digits: &[u8], i: usize) -> u8 {
     let ch = digits.get(i).expect("index out of bounds");
-    *ch - b'0' as u8
+    *ch - b'0'
 }
 
 fn is_digit(string: &str) -> bool {
@@ -145,4 +136,158 @@ fn char_to_digit(ch: u8) -> Option<u8> {
         b'g' | b'h' | b'z' => 9,
         _ => return None
     })
+}
+
+#[derive(Debug, Eq, Clone)]
+struct DigitBytes {
+    even: bool,
+    full: bool,
+    write_index: usize,
+    bytes: [u8; 25],
+}
+
+impl DigitBytes {
+    fn new() -> DigitBytes {
+        DigitBytes { even: true, write_index: 0, bytes: [0; 25], full: false }
+    }
+
+    fn push(&mut self, b: u8) {
+        if self.full {
+            panic!("too many digits pushed")
+        }
+        if self.even {
+            self.bytes[self.write_index] = b;
+        } else {
+            self.bytes[self.write_index] += b << 4;
+            if self.write_index + 1 == self.bytes.len() {
+                // can't increase index anymore otherwise will break Eq and Hash checks
+                self.full = true;
+            } else {
+                self.write_index += 1;
+            }
+        }
+        self.even = !self.even;
+    }
+}
+
+impl PartialEq<Self> for DigitBytes {
+    fn eq(&self, other: &Self) -> bool {
+        self.full == other.full &&
+        self.even == other.even &&
+            self.bytes[0..=self.write_index] == other.bytes[0..=other.write_index]
+    }
+}
+
+impl Hash for DigitBytes {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.bytes[0..=self.write_index].hash(state);
+        self.even.hash(state);
+        self.full.hash(state);
+    }
+}
+
+#[cfg(test)]
+mod digit_bytes_test {
+    use crate::DigitBytes;
+    use std::collections::HashMap;
+
+    #[test]
+    fn can_write() {
+        let mut expected_bytes = [0; 25];
+        let mut bytes = DigitBytes::new();
+        assert_eq!(bytes.bytes, expected_bytes);
+        bytes.push(9);
+        bytes.push(2);
+        expected_bytes[0] = 41;
+        assert_eq!(bytes.bytes, expected_bytes);
+        bytes.push(4);
+        expected_bytes[1] = 4;
+        assert_eq!(bytes.bytes, expected_bytes);
+    }
+
+    #[test]
+    fn can_compare() {
+        let mut a = DigitBytes::new();
+        let mut b = DigitBytes::new();
+        assert_eq!(a, b);
+        assert_eq!(b, a);
+        a.push(3);
+        assert_ne!(a, b);
+        assert_ne!(b, a);
+        b.push(3);
+        assert_eq!(a, b);
+        assert_eq!(b, a);
+        a.push(4);
+        b.push(5);
+        assert_ne!(a, b);
+        assert_ne!(b, a);
+        let mut a = DigitBytes::new();
+        let mut b = DigitBytes::new();
+        a.push(4);
+        b.push(5);
+        assert_ne!(a, b);
+        assert_ne!(b, a);
+    }
+
+    #[test]
+    fn can_see_difference_between_zero_and_e() {
+        let mut a = DigitBytes::new();
+        let mut b = DigitBytes::new();
+        a.push(0);
+        assert_ne!(a, b);
+        assert_ne!(b, a);
+
+        a.push(0);
+        b.push(0);
+        assert_ne!(a, b);
+        assert_ne!(b, a);
+    }
+
+    #[test]
+    fn can_use_in_hash_map() {
+        let mut map = HashMap::<DigitBytes, u32>::new();
+        let a = {
+            let a = DigitBytes::new();
+            map.insert(a.clone(), 1);
+            a
+        };
+        assert!(map.contains_key(&a));
+        assert_eq!(map[&a], 1);
+        let b = {
+            let mut b = a.clone();
+            b.push(4);
+            b
+        };
+        assert!(!map.contains_key(&b));
+
+        let c = {
+            let mut c = b.clone();
+            for i in 1..10 { c.push(i); }
+            c
+        };
+
+        map.insert(b.clone(), 2);
+        assert!(map.contains_key(&b));
+        assert!(!map.contains_key(&c));
+
+        map.insert(c.clone(), 3);
+
+        assert_eq!(map[&a], 1);
+        assert_eq!(map[&b], 2);
+        assert_eq!(map[&c], 3);
+    }
+
+    #[test]
+    fn can_push_max_len() {
+        let mut a = DigitBytes::new();
+        for _ in 0..50 {
+            a.push(0);
+        }
+        assert!(a.full);
+
+        // make sure an instance filled with zero is not equal to an empty one
+        let b = DigitBytes::new();
+        assert_ne!(a, b);
+        assert_ne!(b, a);
+    }
 }
