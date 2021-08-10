8ce90433720004e28e107cd834861f2a381020d9
alianse777 <alianse77@protonmail.com>
2021-08-10 22:59:11 +0500

    use ibig

--- src/rust/phone_encoder/src/main.rs
+++ src/rust/phone_encoder/src/main.rs
@@ -4,15 +4,9 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-use lazy_static::lazy_static;
-use num_bigint::{BigUint, ToBigUint};
+use ibig::{ubig, UBig};
 
-type Dictionary = HashMap<BigUint, Vec<String>>;
-
-lazy_static! {
-    static ref ONE: BigUint = 1.to_biguint().unwrap();
-    static ref TEN: BigUint =10.to_biguint().unwrap();
-}
+type Dictionary = HashMap<UBig, Vec<String>>;
 
 /// Port of Peter Norvig's Lisp solution to the Prechelt phone-encoding problem.
 ///
@@ -21,16 +15,22 @@ lazy_static! {
 fn main() -> io::Result<()> {
     // drop itself from args
     let mut args: Vec<_> = args().skip(1).collect();
-    let words_file: String = if !args.is_empty() { args.remove(0) } else { "tests/words.txt".into() };
-    let input_file: String = if !args.is_empty() { args.remove(0) } else { "tests/numbers.txt".into() };
+    let words_file: String = if !args.is_empty() {
+        args.remove(0)
+    } else {
+        "tests/words.txt".into()
+    };
+    let input_file: String = if !args.is_empty() {
+        args.remove(0)
+    } else {
+        "tests/numbers.txt".into()
+    };
 
     let dict = load_dict(words_file)?;
 
     for line in read_lines(input_file)? {
         if let Ok(num) = line {
-            let digits: Vec<_> = num.chars()
-                .filter(|ch| ch.is_alphanumeric())
-                .collect();
+            let digits: Vec<_> = num.chars().filter(|ch| ch.is_alphanumeric()).collect();
             print_translations(&num, &digits, 0, Vec::new(), &dict)?;
         }
     }
@@ -48,10 +48,10 @@ fn print_translations(
         print_solution(num, &words);
         return Ok(());
     }
-    let mut n = ONE.clone();
+    let mut n = ubig!(1);
     let mut found_word = false;
     for i in start..digits.len() {
-        n = &n * (&*TEN) + &nth_digit(digits, i);
+        n = &n * (ubig!(10)) + &nth_digit(digits, i);
         if let Some(found_words) = dict.get(&n) {
             for word in found_words {
                 found_word = true;
@@ -83,7 +83,8 @@ fn print_solution(num: &str, words: &Vec<&String>) {
     for word in head {
         print!("{} ", word);
     }
-    for word in tail { // only last word in tail
+    for word in tail {
+        // only last word in tail
         println!("{}", word);
     }
 }
@@ -104,42 +105,44 @@ fn load_dict(words_file: String) -> io::Result<Dictionary> {
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-fn word_to_number(word: &str) -> BigUint {
-    let mut n = ONE.clone();
+fn word_to_number(word: &str) -> UBig {
+    let mut n = ubig!(1);
     for ch in word.chars() {
         if ch.is_alphabetic() {
-            n = &n * (&*TEN) + &char_to_digit(ch);
+            n = &n * (ubig!(10)) + char_to_digit(ch);
         }
     }
     n
 }
 
-fn nth_digit(digits: &Vec<char>, i: usize) -> BigUint {
+fn nth_digit(digits: &Vec<char>, i: usize) -> UBig {
     let ch = digits.get(i).expect("index out of bounds");
-    ((*ch as usize) - ('0' as usize)).to_biguint().unwrap()
+    ((*ch as usize) - ('0' as usize)).into()
 }
 
 fn is_digit(string: &str) -> bool {
     string.len() == 1 && string.chars().next().unwrap().is_digit(10)
 }
 
-fn char_to_digit(ch: char) -> u32 {
+fn char_to_digit(ch: char) -> UBig {
     match ch.to_ascii_lowercase() {
-        'e' => 0,
-        'j' | 'n' | 'q' => 1,
-        'r' | 'w' | 'x' => 2,
-        'd' | 's' | 'y' => 3,
-        'f' | 't' => 4,
-        'a' | 'm' => 5,
-        'c' | 'i' | 'v' => 6,
-        'b' | 'k' | 'u' => 7,
-        'l' | 'o' | 'p' => 8,
-        'g' | 'h' | 'z' => 9,
-        _ => panic!("invalid input: not a digit: {}", ch)
+        'e' => ubig!(0),
+        'j' | 'n' | 'q' => ubig!(1),
+        'r' | 'w' | 'x' => ubig!(2),
+        'd' | 's' | 'y' => ubig!(3),
+        'f' | 't' => ubig!(4),
+        'a' | 'm' => ubig!(5),
+        'c' | 'i' | 'v' => ubig!(6),
+        'b' | 'k' | 'u' => ubig!(7),
+        'l' | 'o' | 'p' => ubig!(8),
+        'g' | 'h' | 'z' => ubig!(9),
+        _ => panic!("invalid input: not a digit: {}", ch),
     }
 }
